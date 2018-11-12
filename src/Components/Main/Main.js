9067d38ae0237e3f97ea9063bf2e2abf9de707fb
unknown <mattgvo@gmail.com>
2018-11-12 16:03:06 -0700

    Components-made

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render() {
+      return (
+        <div>
+          Main Route
+        </div>
+  
+      );
+    }
+  }
+  
+  export default Main;
--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import {Switch, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import Login from './Components/Login/Login'
+import Main from './Components/Main/Main'
 
 export default(
   <Switch>
     <Route exact path="/" component={Home}/>
-    <Route  path="/Login" component={Login}/>
+    <Route  path="/login" component={Login}/>
+    <Route  path="/main" component={Main}/>
   </Switch>
 )
