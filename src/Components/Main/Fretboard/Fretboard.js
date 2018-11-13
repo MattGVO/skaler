43751c6f11446bf449f2afdd10763774e35d6f24
unknown <mattgvo@gmail.com>
2018-11-13 11:53:45 -0700

    strings render based on state

--- src/Components/Main/Fretboard/Fretboard.js
+++ src/Components/Main/Fretboard/Fretboard.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import String from './String'
 
 class Fretboard extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      numOfStrings: 2,
+      stringArray: [],
+    }
+  }
   render() {
+    for (let i=0; i < this.state.numOfStrings; i++){
+      this.state.stringArray.push(<String/>)
+    }
+    
     return (
       <div className="Fretboard">
-        <String />
-        <String />
-        <String />
-        <String />
+        <div className="StringContainer">
+        {this.state.stringArray.map( (val,i) => {
+          return val;
+        })}
+        
+        </div>
       </div>
     );
   }
