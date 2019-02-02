0bdd3b0c48004b16953d6c4647baf1664d0c3b70
unknown <mattgvo@gmail.com>
2019-02-02 15:53:00 -0700

    SKALER 2.0

--- src/Components/Home/Login/Login.js
+++ src/Components/Home/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Login.css";
+import "./Login.scss";
 // import { Link } from "react-router-dom";
 import axios from "axios";
 const authUrl = "/auth/";
